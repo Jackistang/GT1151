e72becfa1d210219ae54ccb68d35ec87381805dd
jackis <tangjia.jackis@qq.com>
2020-12-29 17:24:04 +0800

    update gt1151.h

--- inc/gt1151.h
+++ inc/gt1151.h
@@ -10,16 +10,30 @@
 #ifndef APPLICATIONS_GT1151_H_
 #define APPLICATIONS_GT1151_H_
 
-#include <rtthread.h>
+#include "rtconfig.h"
 
+#ifndef GT1151_BUS_NAME 
 #define GT1151_BUS_NAME    "i2c1"
+#endif
 
+#ifndef GT1151_RST_PIN
 #define GT1151_RST_PIN     51
+#endif
+
+#ifndef GT1151_IRQ_PIN
 #define GT1151_IRQ_PIN     108
+#endif
 
+#ifndef GT1151_TOUCH_WIDTH
 #define GT1151_TOUCH_WIDTH  800
-#define GT1151_TOUCH_HEIGHT 480
+#endif
 
+#ifndef GT1151_TOUCH_HEIGHT
+#define GT1151_TOUCH_HEIGHT 480
+#endif
+
+#ifndef GT1151_SUPPORT_POINTS
 #define GT1151_SUPPORT_POINTS 10
+#endif
 
 #endif /* APPLICATIONS_GT1151_H_ */
